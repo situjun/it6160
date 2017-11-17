44f98fc182eeec81345acb446c6db74d5a673aa3
icewating <icewating@hotmail.com>
2017-11-17 13:02:04 +0800

    updated

--- FinalProject/config/commen.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-error_reporting(0);
-$path = dirname(__FILE__);
-require $path.'/global.fuc.php';
-require $path.'/mysql.func.php';
-define("mysql_string_status", get_magic_quotes_gpc());
-?>
